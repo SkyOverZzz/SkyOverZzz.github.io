096a14d8c11f1ae010f4d91d0c132d23fac9ce86
SkyOverZzz <skyoverlear@rambler.ru>
2017-07-08 17:35:18 +0300

    0.01

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+	<body>
+		<form action="submit.php" method="post">
+			<input name="email" placeholder="email"><br>
+			<input name="name" placeholder="имя"><br>
+			<input name="phone" placeholder="телефон"><br>
+			<textarea name="message" placeholder="сообщение"></textarea>
+			<input type="submit" value="отправить сообщение	">
+		</form>
+	</body>
+</html>
